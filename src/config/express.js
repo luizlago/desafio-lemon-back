753f94cda8e81efe4536be7b00ac16bc57b0220d
Luiz Lago <luizlago91@gmail.com>
2024-07-24 18:48:29 -0300

    feat(docs): adicionado swagger como documentação
    - ajustado rota do /public
    - adicionado logo e favicon da lemon
    - adicionado nodemon (yarn dev)

--- src/config/express.js
+++ src/config/express.js
@@ -2,7 +2,15 @@ import express from 'express'
 import compression from 'compression'
 import rateLimit from 'express-rate-limit'
 import morgan from 'morgan'
+import swagger from 'swagger-ui-express'
+import swaggerDoc from './swagger.json' assert { type: 'json' }
 import { apiPath } from './envs.js'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Rotas
 import clientRoutes from '../core/routes/clientRoutes.js'
@@ -41,6 +49,26 @@ app.use(
 app.use(`${apiPath}/cliente`, clientRoutes)
 
 // Diretório publico para acesso de conteúdo estático
-app.use('/public', express.static('./../public'))
+app.use('/public', express.static(`${__dirname}/../public`))
+
+// Customizações visuais do Swagger
+const swaggerOptions = {
+    customSiteTitle: '🍋 Lemon Energy - Documentação API',
+    customfavIcon: `../public/images/favicon.ico`,
+    customCss: `
+        .swagger-ui svg:not(:root), .topbar-wrapper span {
+            display: none;
+        }
+        .topbar-wrapper .link {
+            content: url('/public/images/logo.png');
+        }
+        .swagger-ui .topbar { 
+            background-color: #008059; 
+            border-bottom: 20px solid #ffffff; 
+        }`,
+}
+
+// Rota para Documentação
+app.use('/docs', swagger.serve, swagger.setup(swaggerDoc, swaggerOptions))
 
 export default app
